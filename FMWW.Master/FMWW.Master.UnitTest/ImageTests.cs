beaa2f777cc693717c77ac40f5d8e3886b301cf0
Onoda Yoshiaki <y-onoda@nikki-grp.co.jp>
2017-12-19 11:29:17 +0900

    商品画像取得機能

---
+++ FMWW.Master/FMWW.Master.UnitTest/ImageTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FMWW.Master.UnitTest
+{
+    [TestClass]
+    public class ImageTests
+    {
+        private static readonly Entity.UserAccount UserAccount = Entity.Factory.UserAccount.Load(".user.json");
+
+        [TestMethod]
+        public void TestOpenReadImage()
+        {
+            var page = new FMWW.Master.Image.Ref.Page()
+            {
+                UserAccount = UserAccount
+            };
+            using (var source = page.AsStreamBy("-9733179"))
+            using (var destination = System.IO.File.Open("9733179.jpg", System.IO.FileMode.Create))
+            {
+                // ストリームのコピー
+                source.CopyTo(destination);
+            }
+        }
+
+        [TestMethod]
+        public void TestDownloadImage()
+        {
+            var page = new FMWW.Master.Image.Ref.Page()
+            {
+                UserAccount = UserAccount
+            };
+            page.DownloadBy("-9733180", "9733180.jpg");
+        }
+    }
+}
---
+++ FMWW.Master/Image/Ref/Page.cs
@@ -0,0 +1,42 @@
+﻿using FMWW.Component;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FMWW.Master.Image.Ref
+{
+    public class Page : Http.Page
+    {
+        public Page() : base() { }
+        public Page(Http.Client client) : base(client) { }
+
+        public Stream AsStreamBy(string code)
+        {
+            SignIn();
+            return _Client.OpenRead(Core.Helpers.UrlBuilder.GetImageUrlBy(code));
+        }
+
+        public void DownloadBy(string code, string fileName)
+        {
+            SignIn();
+            this._Client.DownloadFile(
+                        Core.Helpers.UrlBuilder.GetImageUrlBy(code),
+                        fileName);
+//                        String.Format(@"{0}\{1}.jpg", context.WorkDir, code));
+        }
+
+        public void DownloadAsync()
+        {
+            SignedInEventHandler onSignedIn = null;
+            onSignedIn = (a, b) =>
+            {
+                Auth.SignedIn -= onSignedIn;
+                // TODO:非同期で画像ダウンロード
+            };
+            Auth.SignedIn += onSignedIn;
+            SignInAsync();
+        }
+    }
+}
